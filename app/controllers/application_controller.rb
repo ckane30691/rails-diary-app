7e7a6748231e1c419837784975bec5a9da460e68
Cory Kane <ckane30691@gmail.com>
2017-07-07 10:55:47 -0700

    updated application controller to keep track of session count on view_all page; added link to view all from home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper :all
   protect_from_forgery with: :exception
+  before_action :count_requests_in_session
+
+  def count_requests_in_session
+    session[:requests] ||= 0
+    session[:requests] += 1
+  end
 end
